d4b9af38100264f298ebb62638bca3459489c3a9
stojanovskitrajce <53583292+stojanovskitrajce@users.noreply.github.com>
2019-08-02 09:55:38 +0200

    Initial commit: All files

---
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.justjava;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    int numberOfcoffees=2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+
+    public void submitOrder(View view) {
+
+
+        displayPrice(numberOfcoffees*5);
+    }
+public void dda(View view){
+        numberOfcoffees++;
+
+     display(numberOfcoffees);
+
+
+}
+    public void mdd(View view){
+        numberOfcoffees--;
+        display(numberOfcoffees);
+    }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+}
